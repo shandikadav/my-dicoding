90479bca3957ed99678e6ca5da5b99c4915937f0
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2024-09-03 11:29:29 +0700

    menambahkan module

---
+++ belajar-dasar-pemrograman-javascript/modularisasi-harmonis/module/index.mjs
@@ -0,0 +1,5 @@
+import * as user from "./module.mjs";
+
+console.log(user.name);
+console.log(user.favoriteFood);
+user.sayHi(user.name);
---
+++ belajar-dasar-pemrograman-javascript/modularisasi-harmonis/module/module.mjs
@@ -0,0 +1,8 @@
+const name = "Dicoding";
+const favoriteFood = ["pizza", "pasta", "sushi"];
+
+function sayHi(name) {
+  console.log(`Hi, ${name}`);
+}
+
+export { name, favoriteFood, sayHi };
