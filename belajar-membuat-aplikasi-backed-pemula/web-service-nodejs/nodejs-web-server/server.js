fff3e54cf87adbccc146c37667d6ae1008ed06e2
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2024-09-14 13:15:56 +0700

    mengganti response body dengan json

--- belajar-membuat-aplikasi-backed-pemula/web-service-nodejs/nodejs-web-server/server.js
+++ belajar-membuat-aplikasi-backed-pemula/web-service-nodejs/nodejs-web-server/server.js
@@ -9,17 +9,25 @@ const requestListener = (request, response) => {
   if (url === "/") {
     if (method === "GET") {
       response.statusCode = 200;
-      response.end("<h1>Ini adalah homepage</h1>");
+      response.end(
+        JSON.stringify({
+          message: "Ini adalah homepage",
+        })
+      );
     } else {
       response.statusCode = 400;
       response.end(
-        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses dengan ${method} request`,
+        })
       );
     }
   } else if (url === "/about") {
     if (method === "GET") {
       response.statusCode = 200;
-      response.end("<h1>Ini adalah halaman about</h1>");
+      response.end(
+        JSON.stringify({ message: "Halo! ini adalah halaman about" })
+      );
     } else if (method === "POST") {
       let body = [];
 
@@ -31,7 +39,11 @@ const requestListener = (request, response) => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
         response.statusCode = 200;
-        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+        response.end(
+          JSON.stringify({
+            message: `Halo, ${name}! Ini adalah halaman about`,
+          })
+        );
       });
     } else {
       response.statusCode = 400;
@@ -41,7 +53,11 @@ const requestListener = (request, response) => {
     }
   } else {
     response.statusCode = 404;
-    response.end("<h1>Halaman tidak ditemukan!</h1>");
+    response.end(
+      JSON.stringify({
+        message: "Halaman tidak ditemukan!",
+      })
+    );
   }
 };
 
